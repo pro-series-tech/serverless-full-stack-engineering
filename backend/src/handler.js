85e0bf395f562daabe154f6d6ccdf0d7fdc880cf
Victor Santos <victor.uceta@gmail.com>
2019-02-21 21:37:38 -0500

    adding graphql handler and graphql serverless endpoint lambda definition

--- backend/src/handler.js
+++ backend/src/handler.js
@@ -1,14 +1,3 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+exports.graphqlHandler = function graphqlHandler(event, context, callback) {};
