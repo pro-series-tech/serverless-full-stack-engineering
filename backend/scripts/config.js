33f6061468edb81ae4a86d6772b721ce03591e24
Victor Santos <victor.uceta@gmail.com>
2019-02-27 21:20:46 -0500

    adding all needed environment variable and installing dependencies

--- backend/scripts/config.js
+++ backend/scripts/config.js
@@ -1,18 +1,23 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
 /*  this handler will be called by the plugin 'serverless-stack-output' */
-function handler(data, serverless, options) {
-  console.log("Received data", data);
-  console.log("Received options", options);
+function handler (data, serverless, options) {
+  console.log('Received data', data)
+  console.log('Received options', options)
 
   /* build the configuration string */
   let config = [
-  `REACT_APP_ENVIRONMENT=${options.stage}`,
-  `REACT_APP_ENDPOINT=${data.ServiceEndpoint}`
-  ].join("\n");
+    `REACT_APP_ENVIRONMENT=${options.stage}`,
+    `REACT_APP_ENDPOINT=${data.ServiceEndpoint}`,
+    `REACT_APP_USER_POOL_CLIENT_ID=${data.UserPoolClientId}`,
+    `REACT_APP_USER_POOL_ID=${data.UserPoolId}`,
+    `REACT_APP_IDENTITY_POOL_ID=${data.IdentityPoolId}`,
+    `REACT_APP_PICTURES_BUCKET=${data.PicturesBucketName}`,
+    `REACT_APP_FRONT_END_BUCKET=${data.FrontEndBucketName}`
+  ].join('\n')
   /* write configuration to file */
-  fs.writeFileSync(path.join(__dirname, `../../.${options.stage}.env`), config);
+  fs.writeFileSync(path.join(__dirname, `../../.${options.stage}.env`), config)
 }
 /* export the handler */
-module.exports = { handler };
+module.exports = { handler }
--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
             Learn React
           </a>
-          <h4>{process.env.REACT_APP_ENVIRONMENT}</h4>
-          <h5>{process.env.REACT_APP_ENDPOINT}</h5>
+          <p>environment: {process.env.REACT_APP_ENVIRONMENT}</p>
+          <p>endpoint: {process.env.REACT_APP_ENDPOINT}</p>
+          <p>user pool client id: {process.env.REACT_APP_USER_POOL_CLIENT_ID}</p>
+          <p>user pool id: {process.env.REACT_APP_USER_POOL_ID}</p>
+          <p>identity pool id: {process.env.REACT_APP_IDENTITY_POOL_ID}</p>
+          <p>pictures bucket: {process.env.REACT_APP_PICTURES_BUCKET}</p>
+          <p>front end bucket: {process.env.REACT_APP_FRONT_END_BUCKET}</p>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
