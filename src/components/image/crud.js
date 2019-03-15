dbcb872bd1a289c4e76e86f1cfc8f905fa18f640
Victor Santos <victor.uceta@gmail.com>
2019-03-14 21:51:45 -0400

    adding file upload and reader logic

--- src/components/image/crud.js
+++ src/components/image/crud.js
@@ -1,15 +1,17 @@
 
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Modal } from 'antd';
+import { Modal, Upload, Icon, Button } from 'antd';
 import { 
     switchModalVisibility
 } from 'actions/crud';
 
+const initialState = {
+    confirmLoading: false,
+    image: null,
+}
 class ImageCRUD extends Component {
-    state = {
-        confirmLoading: false
-    }
+    state = initialState;
     handleOk = () => {
         this.setState({
             confirmLoading: true
@@ -20,33 +22,76 @@ class ImageCRUD extends Component {
          - show error popup if error
          */
         setTimeout(() => {
-            this.setState({
-                confirmLoading: false
-            });
+            this.setState(initialState);
             this.props.switchModalVisibility(false);
         }, 2000);
     }
     handleCancel = () => {
+        this.setState(initialState);
         this.props.switchModalVisibility(false);
     }
+    handleImageRead = (file) =>{
+        /* the file reader to parse the image file */
+        const reader = new FileReader();
+        /* parse the image */
+        reader.addEventListener('load', () => {
+            this.setState({ image: reader.result });
+        });
+        /* parse as URL data */
+        reader.readAsDataURL(file);
+        /* we want to upload manually */
+        return false;
+    }
+    handleImageRemove = () =>{
+        this.setState({
+            image: null
+        });
+    }
     render() {
+
+        let uploadButton; 
+        if(!this.state.image){
+            uploadButton = (
+                <div style={styles.uploadDiv}>
+                    <Button type="primary" shape="round" icon="upload" size={50}>Browser Picture</Button>
+                </div>
+            );
+        }
         return (
             <Modal
                 centered={true}
                 okText="Create"
                 cancelText="Cancel"
                 visible={this.props.visible}
+                destroyOnClose={true}
                 confirmLoading={this.state.confirmLoading}
                 onOk={this.handleOk}
                 onCancel={this.handleCancel}
             >
-                hello there modafuca
+                <div style={styles.uploadDiv}>
+                    <Upload
+                        name="image"
+                        accept=".png,.jpg,.jpeg"
+                        listType="picture"
+                        className="image-uploader"
+                        multiple={false}
+                        beforeUpload={this.handleImageRead}
+                        onRemove={this.handleImageRemove}
+                        disabled={this.state.image != null}
+                    >
+                        {uploadButton}
+                    </Upload>
+                 </div>
             </Modal>
         )
     }
 }
 const styles = {
-
+    uploadDiv:{
+        paddingTop: 20,
+        width: '100%',
+        textAlign: 'center'
+    }
 };
 const mapStateToProps = (state, ownProps) => {
     return {
