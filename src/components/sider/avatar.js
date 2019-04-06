7ad75c8653cff6c28f2c20d57292b6d0fce3079c
Victor Santos <victor.uceta@gmail.com>
2019-04-06 17:50:59 -0400

    documenting avatar component

--- src/components/sider/avatar.js
+++ src/components/sider/avatar.js
@@ -1,30 +1,47 @@
 
+/* external imports */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { signOut } from 'actions/authentication';
 import { Popconfirm, Avatar, notification } from 'antd';
+/* local imports */
+import { signOut } from 'actions/authentication';
+import ObjectStorage from 'lib/object-storage';
 import { PICTURE_BUCKET } from 'lib/environment';
-import ObjectStorage from 'lib/object-storage'
-
+/* component initial state constant */
 const initialState = {
+    /* the avatar upload count is used to prevent caching
+    of the avatar if reuploaded,i.e. will force the browser to
+    fetch new image from backend */
     avatarUploadCount: 0
 }
+/* the component class */
 class AvatarSection extends Component {
-    state = initialState;
+    /* set the instance initial state as initialState clone */
+    state = {...initialState}
+    /**
+     * Upload input image file to serverless backend. Always returns false to prevent the ant design
+     * file uploader from using the build in upload mechanism, i.e. we want to upload it manually.
+     * @async
+     * @param  {File} file
+     * @returns {boolean}
+     */
     handleAvatarUpload = async(file) => {
-        console.log('file read',)
         /* get the clients with fresh credentials */
         let objectStorageClient = new ObjectStorage(this.props.credentials);
-        /* first we insert the picture */
+        /* first we insert the picture into the serverless backend asynchronously */
         await objectStorageClient.putAvatarPictureFile(file);
-        /* force re render  of avatar */
+        /* force re render of avatar(invalidate cache) */
         this.setState({
             avatarUploadCount: this.state.avatarUploadCount + 1
         });
         /* dont hold the file */
         return false;
     }
-    confirmSignOut = (e) => {
+    /**
+     * Handle sign out event and show notification for user feedback.
+     */
+    confirmSignOut = () => {
+        /* invoke sign out action */
         this.props.signOut();
         /* show removal notification */
         notification.info({
@@ -32,11 +49,20 @@ class AvatarSection extends Component {
             description: `You have been successfully signed out.`
         });
     }
+    /**
+     * Renders the avatar component.
+     * @returns {React.Component}
+     */
     render() {
+        /* get the current credential's identity ID */
         const { identityId } = this.props.credentials;
+        /* build the avatar image URL using ES6 template literal */
         const avatarSrc = `https://s3.amazonaws.com/${PICTURE_BUCKET}/${identityId}/avatar.png?t=${Date.now()}`;
+        /* return a component */
         return (
+            /* The avatar container division */
             <div style={styles.avatarDiv}>
+                {/* The avatar component */}
                 <Avatar 
                     style={styles.avatarPicture}
                     size={64} 
@@ -44,6 +70,7 @@ class AvatarSection extends Component {
                     src={avatarSrc}
                     onClick={()=>{this.fileUpload.click()}}
                 />
+                {/* hidden input button to upload the avatar */}
                 <input 
                     style={styles.fileUpload}
                     type='file' 
@@ -55,6 +82,7 @@ class AvatarSection extends Component {
                 />
                 <br />
                 <br />
+                {/* Pop Confirm to confirm user intent to sing out */}
                 <Popconfirm 
                     placement='bottomLeft'
                     title='Are you sure you want to sign out?' 
@@ -68,6 +96,7 @@ class AvatarSection extends Component {
         )
     }
 }
+/* component styles */
 const styles = {
     avatarDiv: {
         padding: '5px',
@@ -82,14 +111,17 @@ const styles = {
         display: 'none'
     }
 };
+/* redux map state to properties */
 const mapStateToProps = (state, ownProps) => {
     return {
         credentials: state.authentication.credentials
     };
 };
+/* redux map dispatch functions to properties */
 const mapDispatchToProps = {
     signOut
 };
+/* wrap this component into a redux component */
 export default connect(
     mapStateToProps,
     mapDispatchToProps
